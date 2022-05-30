299650c842be1bdd45331f6f22820eeba32f0838
hjyeo9909 <72446622+hjyeo9909@users.noreply.github.com>
2022-05-30 15:19:51 +0900

    Update textfilewriter.c

--- libs/textfilewriter.c
+++ libs/textfilewriter.c
@@ -9,14 +9,7 @@
 #include "textfilewriter.h"
 
 void create_music_titles(FILE* stream){
-    int count = 0;
-    char buffer[MAX_TITLE_SIZE];
-    fscanf(stream, "%d", &count);
-    clear();
-    while (count-->0) {
-        fscanf(stream, "%s", buffer);
-        append_left(strlen(buffer), buffer);
-    }
+	
 }
 
 void read_file(char* file_name){
